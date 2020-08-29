df19ce027efa6b6233f18d693607ec66e0e421f1
Nicolas <nikoalegre@hotmail.com>
2020-08-29 13:57:12 -0300

    Updates

---
+++ precache-manifest.06fba55d38202fc00e2700b3f325276e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73415413040c001426110585f5f6ac23",
+    "url": "/optimizer-pl-io/index.html"
+  },
+  {
+    "revision": "02b5026d83a549ee4adf",
+    "url": "/optimizer-pl-io/static/css/2.2f8ebd95.chunk.css"
+  },
+  {
+    "revision": "0b246494d1f691580335",
+    "url": "/optimizer-pl-io/static/css/main.b47e56a5.chunk.css"
+  },
+  {
+    "revision": "02b5026d83a549ee4adf",
+    "url": "/optimizer-pl-io/static/js/2.d1c9d988.chunk.js"
+  },
+  {
+    "revision": "97f9604cfe86d654385dadd41885f363",
+    "url": "/optimizer-pl-io/static/js/2.d1c9d988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b246494d1f691580335",
+    "url": "/optimizer-pl-io/static/js/main.81b6d46e.chunk.js"
+  },
+  {
+    "revision": "feac02a915d41815534f",
+    "url": "/optimizer-pl-io/static/js/runtime-main.4a332c2c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/optimizer-pl-io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
